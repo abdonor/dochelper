0bf0f576549dda922e2d22fbcd7824a488364c4f
Albert Reis <areis@oceano.com>
2017-07-26 10:24:56 +0200

    first commit

---
+++ src/Abdonor/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Abdonor\Modules\DocHelper;
+
+use Illuminate\Support\ServiceProvider as SProvider;
+
+class ServiceProvider extends SProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
