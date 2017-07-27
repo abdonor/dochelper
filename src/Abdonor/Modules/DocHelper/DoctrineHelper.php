ca08db21c54a1f8baa0454830d60bd01f9cf41e6
Albert Reis <areis@oceano.com>
2017-07-27 10:03:01 +0200

    improvements

--- src/Abdonor/Modules/DocHelper/DoctrineHelper.php
+++ src/Abdonor/Modules/DocHelper/DoctrineHelper.php
@@ -11,9 +11,16 @@ class DoctrineHelper
      * @info You need to instantiate this var in your code with the Doctrine\ORM\QueryBuilder
      * @var QueryBuilder */
     private $query;
-
+    /** @var int Quantity of arguments, internal variable */
     private $qtdArgs = 0;
 
+    const OR = 'OR';
+    const AND = 'AND';
+
+    /**
+     * Example of $range: $range = ['limit' => 100, 'offset' => 20];
+     * @param $range
+     */
     public function addRange($range)
     {
         if ($range) {
@@ -26,16 +33,28 @@ class DoctrineHelper
         }
     }
 
+    /**
+     * @param $column
+     * @param string $order
+     */
     public function addOrderBy($column, $order = 'ASC')
     {
         $this->query->orderBy($column, $order);
     }
 
+    /**
+     * This var is mandatory
+     * @param $query - your repository QueryBuilder
+     */
     public function setQuery($query)
     {
         $this->query = $query;
     }
 
+    /**
+     * It will get your query string from the call
+     * @return array
+     */
     public static function getArrayQuery()
     {
         return parse_query($_SERVER['QUERY_STRING']);
@@ -43,6 +62,7 @@ class DoctrineHelper
 
     /**
      * @param $arrAllowed Are the params allowed to be searched in your query
+     * An example of "$arrAllowed": $allowedParams = ['name' => 'name', 'id' => 'id',];
      * @return array
      */
     public static function getParams($arrAllowed)
@@ -58,6 +78,10 @@ class DoctrineHelper
         return $params;
     }
 
+    /**
+     * It allows you to get the range from your Query String
+     * @return null|['limit' => 100, 'offset' => 20]
+     */
     public static function getRange()
     {
         $queryArr = self::getArrayQuery();
@@ -72,46 +96,85 @@ class DoctrineHelper
 
     public function andLikeOr($params, $nameVar, $nameColumn)
     {
-        if(isset($params[$nameVar])) {
-            $var = $params[$nameVar];
-            $orX = $this->query->expr()->orX();
-            $i = $this->qtdArgs;
-            if (is_array($var)) {
-                foreach ($var as $item) {
-                    $dql = " $nameColumn LIKE ?$i ";
-                    $orX->add($dql);
-                    $this->query->setParameter($i, '%'.$item.'%');
-                    $i++;
-                }
-                $this->query->andWhere($orX);
-            } else {
-                $this->query->andWhere("$nameColumn LIKE ?$i ")->setParameter($i, '%'.$var.'%');
-                $i++;
-            }
-            $this->qtdArgs = $i;
-        }
+        return $this->like($params, $nameVar, $nameColumn, self::OR);
+    }
+
+    public function andLikeAnd($params, $nameVar, $nameColumn)
+    {
+        return $this->like($params, $nameVar, $nameColumn, self::AND);
     }
 
     public function andEqOr($params, $nameVar, $nameColumn)
     {
-        if(isset($params[$nameVar])) {
+        return $this->eq($params, $nameVar, $nameColumn, self::OR);
+    }
+
+    public function eq($params, $nameVar, $nameColumn, $operator)
+    {
+        if (isset($params[$nameVar])) {
             $var = $params[$nameVar];
-            $orX = $this->query->expr()->orX();
+            if ($operator == self::AND) {
+                $opX = $this->query->expr()->andX();
+                $op = self::AND;
+            } elseif ($operator == self::OR) {
+                $opX = $this->query->expr()->orX();
+                $op = self::OR;
+            }
             $i = $this->qtdArgs;
             if (is_array($var)) {
                 foreach ($var as $item) {
                     $dql = " $nameColumn = ?$i ";
-                    $orX->add($dql);
+                    $opX->add($dql);
                     $this->query->setParameter($i, $item);
                     $i++;
                 }
-
-                $this->query->andWhere($orX);
+                $this->query->andWhere($opX);
             } else {
                 $this->query->andWhere("$nameColumn = ?$i")->setParameter($i, $var);
                 $i++;
             }
             $this->qtdArgs = $i;
         }
+    }
+
+    private function like($params, $nameVar, $nameColumn, $operator)
+    {
+        if (isset($params[$nameVar])) {
+            $var = $params[$nameVar];
+            if ($operator == self::AND) {
+                $opX = $this->query->expr()->andX();
+                $op = self::AND;
+            } elseif ($operator == self::OR) {
+                $opX = $this->query->expr()->orX();
+                $op = self::OR;
+            }
+
+            $i = $this->qtdArgs;
+            if (is_array($var)) {
+                foreach ($var as $item) {
+                    $dql = '';
+                    if (is_array($nameColumn)) {
+                        $op1 = '';
+                        foreach ($nameColumn as $nameColumnItem) {
+                            $dql .= " $op1 $nameColumnItem LIKE ?$i ";
+                            $op1 = $op;
+                        }
+                    } else {
+                        $dql = " $nameColumn LIKE ?$i ";
+                    }
+
+                    $opX->add($dql);
+                    $this->query->setParameter($i, '%'.$item.'%');
+                    $i++;
+                }
+                $this->query->andWhere($opX);
+            } else {
+                $this->query->andWhere("$nameColumn LIKE ?$i ")->setParameter($i, '%'.$var.'%');
+                $i++;
+            }
+            $this->qtdArgs = $i;
+        }
+
+        return $this->query;
     }
 }
