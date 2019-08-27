f217c141016d15544ae74f8d40198f50eb3599f2
Albert Abdonor <albert.abdonor@gmail.com>
2019-08-27 12:59:57 +0200

    Update DocHelper.php

--- src/Abdonor/Modules/DocHelper/Facade/DocHelper.php
+++ src/Abdonor/Modules/DocHelper/Facade/DocHelper.php
@@ -4,6 +4,17 @@ namespace Abdonor\Modules\DocHelper\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class DocHelper
+ * @package Abdonor\Modules\DocHelper\Facade
+ * @method static array getParams()
+ * @method static array setParamsByInput()
+ * @method static array setParamsNoQS()
+ * @method static array getRange()
+ * @method static array getOrderBy()
+ * @method static array search()
+ * @method static array getRequest()
+ */
 class DocHelper extends Facade
 {
     /**
