766a8d02ba05e574a4dd4803723aecbdf95b264e
Albert Abdonor <albert.abdonor@gmail.com>
2019-08-27 14:05:50 +0200

    Update DocHelper.php

--- src/Abdonor/Modules/DocHelper/Facade/DocHelper.php
+++ src/Abdonor/Modules/DocHelper/Facade/DocHelper.php
@@ -7,13 +7,23 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class DocHelper
  * @package Abdonor\Modules\DocHelper\Facade
- * @method static array getParams()
- * @method static array setParamsByInput()
- * @method static array setParamsNoQS()
+ * @method static addRange($range)
+ * @method static array setRange()
  * @method static array getRange()
+ * @method static array getParams($arrAllowed)
+ * @method static array getParamsNoQS($arrAllowed, $queryArr = [])
  * @method static array getOrderBy()
- * @method static array search($array = [])
- * @method static array getRequest()
+ * @method static array getParamsInput($arrAllowed, $queryArr = [])
+ * @method static andLikeOr($params, $nameVar, $nameColumn)
+ * @method static andStartWithOr($params, $nameVar, $nameColumn)
+ * @method static andEndWithAnd($params, $nameVar, $nameColumn)
+ * @method static andLikeAnd($params, $nameVar, $nameColumn)
+ * @method static andEqOr($params, $nameVar, $nameColumn)
+ * @method static andNull($params, $nameVar, $nameColumn)
+ * @method static andNotNull($params, $nameVar, $nameColumn)
+ * @method static andBetween($params, $nameVar1, $nameVar2, $nameColumn)
+ * @method static andIn($params, $nameVar1, $nameColumn)
+ * @method static Search search($array = [])
  */
 class DocHelper extends Facade
 {
